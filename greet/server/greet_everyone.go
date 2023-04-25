e86dc4cc67cb9b5e8d2c16c30c85adc4157b6416
Joe Cui <xiaozhou.cui@gmail.com>
2023-04-25 12:44:05 +1000

    Add bi-directional-streaming API Server for greet

---
+++ greet/server/greet_everyone.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/XiaozhouCui/grpc-go-app/greet/proto"
+)
+
+func (s *Server) GreetEveryone(stream pb.GreetService_GreetEveryoneServer) error {
+	log.Panicln("GreetEveryone function was invoked")
+
+	// infinite loop: keep receiving client stream until EOF
+	for {
+		req, err := stream.Recv()
+
+		// stop if client stream has ended
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v\n", err)
+		}
+
+		res := "Hello " + req.FirstName + "!"
+
+		// send back the result as a stream
+		err = stream.Send(&pb.GreetResponse{
+			Result: res,
+		})
+
+		if err != nil {
+			log.Fatalf("Error while sending data to client: %v\n", err)
+		}
+	}
+}
